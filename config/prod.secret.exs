f04c502b7de4cfa2a170d8352719ecb395533f12
kelvinst <kelvin.stinghen@gmail.com>
2020-05-13 15:58:30 -0300

    Endpoint should not use https, just the url should point to  it

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -12,7 +12,7 @@ secret_key_base =
     """
 
 config :market_web, MarketWeb.Endpoint,
-  https: [
+  http: [
     port: String.to_integer(System.get_env("PORT") || "4000"),
     transport_options: [socket_opts: [:inet6]]
   ],
